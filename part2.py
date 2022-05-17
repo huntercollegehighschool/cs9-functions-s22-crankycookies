9d998f4de871c776d209d24d24a1e3e357041f30
Lynn Chen <103528440+crankycookies@users.noreply.github.com>
2022-05-17 17:59:10 +0000

    stuff ;-; not done yet

--- part2.py
+++ part2.py
@@ -8,5 +8,7 @@ The formula for converting from fahrenheit to celsius:
 C = (F - 32) * 5/9
 """
 
-def celsius(f):  # do not change this line
-  pass  # delete the word pass when you start writing your code
+def celsius(f):
+  f = float(f)# do not change this line
+  f = (f-32)*5/9  # delete the word pass when you start writing your code
+  return f
